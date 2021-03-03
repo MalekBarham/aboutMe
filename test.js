8f096351150396e39e1124ff8d338d7dbb70f1cc
Malek Barham <malek50000@gmail.com>
2021-03-04 01:33:44 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
 
+'use strict'
+
+let userName=prompt('please provide your name ');
+
+while (!userName) {
+    userName=prompt('can you tell us about you name');
+    
+}
+if (userName !=='stop') {
+    alert('welcome' + userName + 'lets get started');
+
+}
+
+// 
+
