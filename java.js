734388a800be8d38fa0a69402dc7fe9ed6440cc1
Malek Barham <malek50000@gmail.com>
2021-03-03 08:57:09 +0200

    Update java.js
    add few files

--- java.js
+++ java.js
@@ -1,17 +1,19 @@
 'use strict'
+
 let userName=prompt("can you tell us about your name ?");
-alert(  userName      +    "welcome to our website"  );
+alert( "welcome " + userName);
+let favouritecolor=parseInt(prompt(" is my favourite  color is red? \n (Yes-no)"));
+let meal=parseInt(prompt("Is Mansaf my favorite meal? \n (Yes-no)"));
+let answer2=parseInt(prompt(" Did i study accounting  ?  \n (Yes-no) "));
+let answer3=parseInt(prompt(" is my favorit color is red ?  \n (Yes-no)"));
+let q5=parseInt(prompt(" did you like my website? \n (Yes-no)"));
+let n1=parseInt(prompt("can you guess my name ? \n  [ Malek-Mohummad-Yahya-samer  ]"));
 
 
-// gessing game 
 
-let favouritecolor=prompt(" is my favourite  color is red? (Yes-no)");
-let meal=prompt("Is Mansaf my favorite meal?(Yes-no)");
-let answer2=prompt(" Did i study accounting  ? (Yes-no) ");
-let answer3=prompt(" is my favorit color is red ? (Yes-no)");
-let q5=prompt(" did you like my website? (Yes-no)");
 
-if (favouritecolor.toUpperCase()=="YES" || favouritecolor.toUpperCase() =="Y" ) {
+
+if (favouritecolor.toUpperCase()=="YES"     || favouritecolor.toLowerCase()=="Y"   ) {
     console.log("Thats Correct answer");
 }
 else if (favouritecolor.toLowerCase()=="no" || favouritecolor.toLowerCase()=="n") {
@@ -56,4 +58,47 @@ else if (q5.toLowerCase()=="no" || q5.toLowerCase()=="n") {
 else {
     alert("wrong input, please try agin");
 }
-document.write( favouritecolor ,  meal  , answer2  ,  answer3  ,  q5   ,     ("this is your answer"));
+let age;
+let counter = 0;
+let a = 0;
+
+do { 
+    age = parseInt(prompt("guess my age  you have five chanses "));
+
+    if (age===26) {
+        console.log("yor answer is correct ");
+        alert("your answer is crrect");
+        a = 1;
+    } 
+     else if (age <26) {
+         alert("your answer is to low from the correct answer ");
+         counter +=1;
+     }
+    else if (age > 26) {
+        alert("your answer is to high from the correct answer");
+        counter +=1;
+    }  else {
+        alert("please enter a number");
+        counter +=1;
+    }
+    if (counter===4) {
+        alert("you toke all chansess to answer  -  the correct answer is 26");
+        a =1;
+    }
+} while (a === 0);
+
+
+switch (n1) {
+    case "yahya":
+        console.log("this is wrong name");
+        break;
+     case "mouhmmed":
+     console.log("this is wrong name");
+     break;
+     case "samer":
+         console.log("please try again");
+         break;
+    default:
+    console.log("this is correct answer");
+        break;
+} 
